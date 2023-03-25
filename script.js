a08896f856ce869351a4358b9261b83eda6bbdd4
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-03-26 01:26:14 +0300

    Update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const menu = [
       "Toptan-250ml",
     ],
     price: ["₺70.00", "₺52.50", "₺40.00", "₺30.00"],
-    img: "https://picsum.photos/200/300",
+    img: ["https://picsum.photos/200/300", "./imgs/ada2.png"],
   },
   {
     id: 2,
@@ -21,18 +21,630 @@ const menu = [
       "Toptan-250ml",
     ],
     price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
-    img: "https://picsum.photos/200/300",
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 3,
+    title: "Ananas Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 4,
+    title: "Armut Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 5,
+    title: "Ayva Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 6,
+    title: "Bal Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 7,
+    title: "Biberiye Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 8,
+    title: "Çilek Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 9,
+    title: "Elma Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 10,
+    title: "Enginar Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 11,
+    title: "Erik Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 12,
+    title: "Frenk Yemişi Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 13,
+    title: "Gelincik Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 14,
+    title: "Gilaboru Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 15,
+    title: "Hanımeli Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 16,
+    title: "Hatmi Çiçeği Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 17,
+    title: "Hayıt Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 18,
+    title: "Hurma Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 19,
+    title: "Hünnap Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 20,
+    title: "Ihlamur Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 21,
+    title: "Isırgan Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 22,
+    title: "Isparta Gülü Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 23,
+    title: "Itır Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 24,
+    title: "İğde Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 25,
+    title: "İncir Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 26,
+    title: "Kabak Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 27,
+    title: "Kadife Çiçeği Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 28,
+    title: "Keçi Boynuzu Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 29,
+    title: "Kekik Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 30,
+    title: "Kızılcık Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 31,
+    title: "Kiraz Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 32,
+    title: "Kivi Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 33,
+    title: "Kozalak Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 34,
+    title: "Kudret Narı Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 35,
+    title: "Kuşburnu Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 36,
+    title: "Lavanta Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 37,
+    title: "Limon Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 38,
+    title: "Limon-Portakal Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 39,
+    title: "Mandalin Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 40,
+    title: "Melisa Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 41,
+    title: "Mor Salkım Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 42,
+    title: "Muşmula Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 43,
+    title: "Nane Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 44,
+    title: "Nar Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 45,
+    title: "Pancar Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 46,
+    title: "Reyhan Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 47,
+    title: "Şeftali Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 48,
+    title: "Şeftali Çiçeği Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 49,
+    title: "Üzüm Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 50,
+    title: "Yaban Mersini Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 51,
+    title: "Yeşil Çay Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 52,
+    title: "Yeşil Zeytin Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
+  },
+  {
+    id: 53,
+    title: "Zeytin Yaprağı Sirkesi",
+    category: [
+      "Perakende-500ml",
+      "Perakende-250ml",
+      "Toptan-500ml",
+      "Toptan-250ml",
+    ],
+    price: ["₺60.00", "₺45.00", "₺35.00", "₺26.25"],
+    img: ["https://picsum.photos/200/300", "https://picsum.photos/100/150"],
   },
 ];
 
-const sayfaButton = document.querySelector("#anaSayfa");
-const sirkeButton = document.querySelector("#sirke");
+const sayfaButton = document.querySelector("#anaSayfa"); //AnaSayfa Butonu
+const sirkeButton = document.querySelector("#sirke"); //Sirkeler
 const iletisimButton = document.querySelector("#iletisim");
 const bodybutton = document.querySelector(".section-center");
 
-sayfaButton.addEventListener("click", run);
-sirkeButton.addEventListener("click", run);
-iletisimButton.addEventListener("click", run);
+// sayfaButton.addEventListener("click", run);
+sirkeButton.addEventListener("click", sirkeFunc);
+// iletisimButton.addEventListener("click", run);
 
 const sirke = [];
 
@@ -40,43 +652,52 @@ menu.forEach((item) => {
   sirke.push(item);
 });
 
+//Card Function
 function createArray(array) {
   array.map((item) => {
+    //Creating Card
     let div1 = document.createElement("div");
     let parentElement = document.querySelector(".section-center");
-    div1.className = "menu-items col-lg-6 col-sm-12 mt-5";
+    div1.className = "col-md-6 col-sm-12 mt-4 ms-0";
     parentElement.appendChild(div1);
 
+    //Border
     let mInfo = document.createElement("div");
-    mInfo.className = "menu-info border border-secondary rounded";
+    mInfo.className = "border border-secondary ms-0";
     div1.appendChild(mInfo);
 
+    //Tittle
     let mTitle = document.createElement("div");
-    mTitle.className = "menu-title ms-2";
+    mTitle.className = "ps-2 border-bottom border-secondary pt-2";
     mInfo.appendChild(mTitle);
-
-    let div2 = document.createElement("div");
-    div2.className = "row";
-    mInfo.appendChild(div2);
-
-    let mImg = document.createElement("img");
-    mImg.className = "photo col-4 pb-2 ms-2 rounded";
-    mImg.src = item.img;
-    div2.appendChild(mImg);
 
     let titleInfo = document.createElement("h4");
     titleInfo.innerHTML = item.title;
     mTitle.appendChild(titleInfo);
 
+    //Image and Inside Buttons Part
+    let div2 = document.createElement("div");
+    div2.className = "row";
+    mInfo.appendChild(div2);
+
+    //Creating Image
+    let mImg = document.createElement("img");
+    mImg.className = "photo col-4 ms-1";
+    mImg.src = item.img[0];
+    div2.appendChild(mImg);
+    //Changing img with hovering
+    mImg.addEventListener("mouseenter", (e) => (mImg.src = item.img[1]));
+    mImg.addEventListener("mouseleave", (e) => (mImg.src = item.img[0]));
+
+    //Inside buttons Part
     let div3 = document.createElement("div");
     div3.className = "buttons col-8 d-flex flex-column justify-content-center";
     div2.appendChild(div3);
 
-    console.log(item.category.length);
-
+    //Buttons hover Effect Part
     item.category.forEach((cat) => {
       let button = document.createElement("button");
-      button.className = `btn btn-outline-primary`;
+      button.className = `btn btn-outline-dark btn-text`;
       button.innerHTML = cat;
       div3.appendChild(button);
       button.addEventListener(
@@ -88,12 +709,13 @@ function createArray(array) {
   });
 }
 
-function run() {
+//Sirke function
+function sirkeFunc() {
   while (bodybutton.hasChildNodes()) {
     bodybutton.removeChild(bodybutton.firstChild);
   }
   if (this.id == "sirke") {
     createArray(sirke);
-    sirkeButton.className = "btn btn-outline-dark";
+    sirkeButton.className = "btn btn-outline-dark button";
   }
 }
